7557119dad0679aaf3b982957a6172b6636920b2
farcs <chris.farthing@outlook.com>
2020-09-18 11:23:00 -0400

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Item from './TodoItem'
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <section>
+        <h2>Todo List</h2>
+        <Item />
+        <Item />
+        <Item />
+      </section>
+    )
+  }
+}
