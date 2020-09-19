e2b0211a314136979463a7253a64ebc4d5703cad
farcs <chris.farthing@outlook.com>
2020-09-19 15:10:51 -0400

    Implement TodoList Component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
-import Item from './TodoItem'
+import TodoItem from './TodoItem'
 
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props
     return (
-      <section>
-        <h2>Todo List</h2>
-        <Item />
-        <Item />
-        <Item />
-      </section>
+      <ul className='list-group my-5'>
+        <h3 className='text-capitalize text-center'>Todo List</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.key}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            ></TodoItem>
+          )
+        })}
+        <button
+          type='button'
+          className='btn btn-danger btn-block text-uppercase bt-5'
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     )
   }
 }
